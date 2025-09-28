882db9012988409fe96393844fc84d8f220d869c
jackbisceglia <jackbisceglia2000@gmail.com>
2025-09-28 00:35:19 -0400

    fix: specify tsconfigs at package level to resolve paths

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -2,4 +2,11 @@
 import base from "../../eslint.config.js";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig(base);
+export default defineConfig(...base, {
+  languageOptions: {
+    parserOptions: {
+      project: "./tsconfig.json",
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+});
